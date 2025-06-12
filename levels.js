5ecfe37316de26e173c77d03f87aea3de3a83fcd
Fathan Faqih Ali <70702333+Shiro291@users.noreply.github.com>
2025-06-13 00:48:00 +0700

    Update levels.js

--- levels.js
+++ levels.js
@@ -35,8 +35,9 @@ options: [
 // 5. Quiz C3: Fotosintesis
     {
      type: 'quiz',
-question: 'Perhatikan gambar di bawah ini! assets/Picture1-1749748241692.jpg Organisme yang dapat menggunakan energi yang tersimpan dalam gambar di atas adalah ... ',
-
+question: 'Perhatikan gambar di bawah ini!', 
+imageUrl: 'assets/Picture1-1749748241692.jpg', 
+question: 'Organisme yang dapat menggunakan energi yang tersimpan dalam gambar di atas adalah ... ',
 options: [
   {    text: 'Tumbuhan melalui proses fotosintesis',    correct: true,  },
   {    text: 'Hewan melalui proses fotosintesis',    correct: false,  },
